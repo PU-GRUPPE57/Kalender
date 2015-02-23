f4518a3cd88e4244117f991e9f5cde6928ea4a8a
sindrefl <sindre.flood@gmail.com>
2015-02-23 13:56:42 +0100

    kjasnfkjas

--- Kalender/src/test/HEYHEY.java
+++ Kalender/src/test/HEYHEY.java
@@ -1,5 +1,11 @@
 package test;
 
 public class HEYHEY {
- public final String forentest = "gnksdjnks";
+public final String forentest = "gnksdjnks";
+
+
+public String søkdmgakldna(){
+	return "haha";
+}
+
 }
